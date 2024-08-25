9fb6fdfbbc0d8172ce470d8718102597ee1b74fd
Aman Ullah <amanu@infinite.com>
2024-08-25 15:05:41 +0530

    Another Initial Commit

---
+++ ECommerceAppDemo/ECommerceAppDemo.Web/Scripts/toggleButton.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const toggle = document.getElementById('toggleDarkMode');
+    const body = document.querySelector('body');
+
+    toggle.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+
+        if (body.classList.contains('dark-mode')) {
+            body.style.background = 'black';
+            body.style.color = 'white';
+        } else {
+            body.style.background = 'white';
+            body.style.color = 'black';
+        }
+    });
+});
